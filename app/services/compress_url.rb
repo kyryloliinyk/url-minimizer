39839362e4504dd1b40f195a046e4781e94446ed
kyryloliinyk <kyryloliinyk@gmail.com>
2024-09-27 16:33:14 +0300

    Revise url copressing

--- app/services/compress_url.rb
+++ app/services/compress_url.rb
@@ -3,6 +3,7 @@
 # Responsible for creating short version of origin url
 class CompressUrl
   extend Callable
+  include Rails.application.routes.url_helpers
 
   def initialize(url)
     @url = url
@@ -11,12 +12,9 @@ class CompressUrl
   def call
     raise(ArgumentError, 'invalid url') unless url =~ URI::DEFAULT_PARSER.make_regexp
 
-    parsed_url = URI.parse(url)
-    scheme = parsed_url.scheme
-    full_host = url.remove("#{scheme}://")
-    short_host = Base64.urlsafe_encode64(full_host)
+    encoded_url = Base64.urlsafe_encode64(url)
 
-    "#{scheme}://#{short_host}"
+    "#{Rails.application.config.base_url}#{api_v1_minimized_urls_path}/#{encoded_url}"
   end
 
   private
--- app/services/decompress_url.rb
+++ app/services/decompress_url.rb
@@ -3,6 +3,7 @@
 # Responsible for creating origin version of url from short version
 class DecompressUrl
   extend Callable
+  include Rails.application.routes.url_helpers
 
   def initialize(url)
     @url = url
@@ -11,15 +12,16 @@ class DecompressUrl
   def call
     raise ArgumentError unless url =~ URI::DEFAULT_PARSER.make_regexp
 
-    parsed_url = URI.parse(url)
-    scheme = parsed_url.scheme
-    short_host = url.remove("#{scheme}://")
-    original_host = Base64.decode64(short_host)
-
-    "#{scheme}://#{original_host}"
+    Base64.decode64(compressed_url)
   end
 
   private
 
   attr_reader :url
+
+  def compressed_url
+    url
+      .remove("#{Rails.application.config.base_url}")
+      .remove(api_v1_minimized_urls_path)[1..]
+  end
 end
