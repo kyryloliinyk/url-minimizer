48cbddf725b9af1ec73046f80a461af2f96c79ba
kyryloliinyk <kyryloliinyk@gmail.com>
2024-09-27 09:22:23 +0300

    Update MinimizedUrlsController namespacing

--- app/controllers/api/v1/minimized_urls_controller.rb
+++ app/controllers/api/v1/minimized_urls_controller.rb
@@ -1,33 +1,37 @@
 # frozen_string_literal: true
 
-# Controller to interact with MinimizedUrl entitties
-class MinimizedUrlsController < ApplicationController
-  def show
-    origin_url = Resources::MinimizedUrl::GetOriginFromShort.(show_params)
-    redirect_to origin_url, status: :moved_permanently
-  rescue ArgumentError
-    head :bad_request
-  end
+module Api
+  module V1
+    # Controller to interact with MinimizedUrl entitties
+    class MinimizedUrlsController < ApplicationController
+      def show
+        origin_url = Resources::MinimizedUrl::GetOriginFromShort.(show_params)
+        redirect_to origin_url, status: :moved_permanently
+      rescue ArgumentError
+        head :bad_request
+      end
 
-  def create
-    minized_url = Resources::MinimizedUrl::Create.(create_params)
-    render json: serialized(MinimizedUrlSerializer, minized_url), status: :ok
-  rescue ArgumentError
-    head :bad_request
-  end
+      def create
+        minized_url = Resources::MinimizedUrl::Create.(create_params)
+        render json: serialized(MinimizedUrlSerializer, minized_url), status: :ok
+      rescue ArgumentError
+        head :bad_request
+      end
 
-  def statistcs
-    statistcs = Resources::MinimizedUrl::GetStatistics.()
-    render json: statistcs, status: :ok
-  end
+      def statistcs
+        statistcs = Resources::MinimizedUrl::GetStatistics.()
+        render json: statistcs, status: :ok
+      end
 
-  private
+      private
 
-  def show_params
-    params.require(:short)
-  end
+      def show_params
+        params.require(:short)
+      end
 
-  def create_params
-    params.require(:minimized_url).require(:origin)
+      def create_params
+        params.require(:minimized_url).require(:origin)
+      end
+    end
   end
 end
